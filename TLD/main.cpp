6a297bc9971f1fc01652081690f1c3c85cc3e206
Orthocenter <chenyuxin.mail@gmail.com>
2014-11-04 12:20:39 +0800

    Initial Commit

---
+++ TLD/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TLD
+//
+//  Created by 陈裕昕 on 11/4/14.
+//  Copyright (c) 2014 Fudan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
