e2f4c401c68ae44d8e61655ade23142c4c1559e6
Orthocenter <chenyuxin.mail@gmail.com>
2014-11-05 14:47:06 +0800

    Finish NNClassifier

---
+++ TLD/NNClassifier.cpp
@@ -0,0 +1,9 @@
+//
+//  NNClassifier.cpp
+//  TLD
+//
+//  Created by 陈裕昕 on 11/4/14.
+//  Copyright (c) 2014 Fudan. All rights reserved.
+//
+
+#include "NNClassifier.h"
---
+++ TLD/NNClassifier.h
@@ -0,0 +1,14 @@
+//
+//  NNClassifier.h
+//  TLD
+//
+//  Created by 陈裕昕 on 11/4/14.
+//  Copyright (c) 2014 Fudan. All rights reserved.
+//
+
+#ifndef __TLD__NNClassifier__
+#define __TLD__NNClassifier__
+
+#include <stdio.h>
+
+#endif /* defined(__TLD__NNClassifier__) */
