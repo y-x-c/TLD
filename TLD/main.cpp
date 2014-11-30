7e4b86688cbb48a2e8dd6b10dd68b1fe4b2fc2e3
Orthocenter <chenyuxin.mail@gmail.com>
2014-11-30 08:59:42 +0800

    Added a example : video stabilization.

--- TLD/main.cpp
+++ TLD/main.cpp
@@ -174,13 +174,86 @@ void testOnCamera()
 
 void stabilize()
 {
-    
+    string dir("/home/cyx/Developments/OpenTLD/_input/06_car/");
+    //VideoController videoController(dir, ".jpg");
+    VideoController videoController("/home/cyx/Desktop/3.m4v");
+    ViewController viewController(&videoController);
+
+    videoController.readNextFrame();
+
+    Rect rect;
+    bool drawBBox = true;
+    if(drawBBox)
+    {
+        rect = viewController.getRect();
+    }
+    else
+    {
+        string initFilename(dir + "init.txt");
+        FILE *fin = fopen(initFilename.c_str(), "r");
+        int tlx, tly, brx, bry;
+        fscanf(fin, "%d,%d,%d,%d", &tlx, &tly, &brx, &bry);
+
+        rect = Rect(Point2d(tlx, tly), Point2d(brx, bry));
+        fclose(fin);
+    }
+
+    cerr << "Input Rect : " <<  rect << endl;
+
+    TLD tld(videoController.getCurrFrame(), rect);
+
+    int width = videoController.frameSize().width;
+    int height = videoController.frameSize().height;
+
+    Point2i centerOut;
+    centerOut.x = width / 2;
+    centerOut.y = height / 2;
+
+    while(videoController.readNextFrame())
+    {
+        tld.setNextFrame(videoController.getCurrFrame());
+        int status = tld.track();
+
+        if(status == TLD_TRACK_SUCCESS)
+        {
+            Point2i centerIn;
+            centerIn.x = (tld.getBB().tl().x + tld.getBB().br().x) / 2;
+            centerIn.y = (tld.getBB().tl().y + tld.getBB().br().y) / 2;
+
+            Mat output(videoController.frameSize(), CV_8UC3, Scalar::all(0));
+
+            int dx = centerOut.x - centerIn.x;
+            int dy = centerOut.y - centerIn.y;
+
+            Point2i inTl(max(0, -dx), max(0, -dy));
+            Point2i inBr(min(width, width - dx), min(height, height - dy));
+            Point2i outTl(max(0, dx), max(0, dy));
+            Point2i outBr(min(width, width + dx), min(height, height + dy));
+            Rect ROIIn(inTl, inBr);
+            Rect ROIOut(outTl, outBr);
+
+            videoController.getCurrFrame()(ROIIn).copyTo(output(ROIOut));
+            imshow("Stabilize", output);
+            waitKey(1);
+        }
+        else
+        {
+            Mat output(videoController.frameSize(), CV_8U, Scalar::all(0));
+
+            imshow("Stabilize", output);
+            waitKey(1);
+        }
+
+        viewController.refreshCache();
+        viewController.drawRect(tld.getBB(), COLOR_GREEN, 2);
+        viewController.showCache();
+    }
 }
 
 int main(int argc, char *argv[])
 {
     //testOnTLDDataset();
-    testOnVideo();
+    //testOnVideo();
     //testOnCamera();
     
     stabilize();
